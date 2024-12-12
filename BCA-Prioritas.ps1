156f8f750e3beb2c296eced3cee5423296ef6cd0
DNS <37093+DNS@users.noreply.github.com>
2024-12-12 11:58:49 +0700

    prettify

--- BCA-Prioritas.ps1
+++ BCA-Prioritas.ps1
@@ -1,25 +1,30 @@
-﻿# pip install pdfminer.six
+﻿<#
+Dependencies:
+	winget winget install JernejSimoncic.Wget
+	pip install pdfminer.six
+#>
 
 $base = 'https://prioritas.bca.co.id'
 $page = 'https://prioritas.bca.co.id/en/Wealth-Management/Market-Insight/House-View-Report'
 
 $a = Invoke-WebRequest $page
-$pdf_url = $base + $($a.Links.href | sls '\.pdf')[0]
+$pdf_url = $base + $($a.Links.href | Select-String '\.pdf')[0]
 $pdf_filename = Split-Path $pdf_url -Leaf
-wget -cnd --tries=1 $pdf_url
-python C:\Users\dns\AppData\Roaming\Python\Python311\Scripts\pdf2txt.py $pdf_filename | Out-File "$pdf_filename.txt" -Encoding utf8BOM
+wget -cnd --tries=1 $pdf_url | Out-Null
+python "C:\Users\$env:USERNAME\AppData\Roaming\Python\Python311\Scripts\pdf2txt.py" $pdf_filename | Out-File "$pdf_filename.txt" -Encoding utf8BOM | Out-Null
 
-$f = gc "$pdf_filename.txt" -Raw
-
-
+$f = Get-Content "$pdf_filename.txt" -Raw
 $f -replace 'ΓÇô', '—' | Out-File "$pdf_filename.txt" -Encoding utf8BOM
 
-#$pdf_filename
-gc "$pdf_filename.txt" | sls '(Cash/Deposit|Fixed Income (USD|IDR)|Equity (USD|IDR)).+?—'
+Clear-Host
+"____________________________"
+$pdf_filename
+''
+Get-Content "$pdf_filename.txt" | Select-String '(Cash/Deposit|Fixed Income (USD|IDR)|Equity (USD|IDR)).+?—' -Raw
 
 
-#ri $pdf_filename -Force
-#ri "$pdf_filename.txt" -Force
+#Remove-Item $pdf_filename -Force
+#Remove-Item "$pdf_filename.txt" -Force
 
 <#
 DM	Developed Market
