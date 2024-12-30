6ce13ec839fc6332881f536260a2cf2a07b21bce
DNS <37093+DNS@users.noreply.github.com>
2024-12-30 08:08:16 +0700

    -

--- Ticker.ps1
+++ Ticker.ps1
@@ -1,2 +1,6 @@
-﻿https://generic709.herokuapp.com/stockc/${singleticker}
+﻿# https://generic709.herokuapp.com/stockc/${singleticker}
+
+Invoke-RestMethod "https://generic709.herokuapp.com/stockc/$($args[0])"
+
+
 
