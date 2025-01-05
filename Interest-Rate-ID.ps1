eb693f95f90ddc8a768673f1e4333cc607394c6c
DNS <37093+DNS@users.noreply.github.com>
2025-01-05 21:30:11 +0700

    A: Interest-Rate-ID.ps1

--- Interest-Rate-ID.ps1
+++ Interest-Rate-ID.ps1
@@ -1,1 +1,6 @@
 ﻿# Interest-Rate-ID.ps1
+
+$a = iwr https://www.bi.go.id/id/statistik/indikator/BI-Rate.aspx
+$a.Content -match '<td class="text-center">(.*?) %</td>' | Out-Null; "$($Matches[1])%"
+
+
