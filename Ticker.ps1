c22a0e7dd37cf6b2064eac1c51a8b8b88d645095
DNS <37093+DNS@users.noreply.github.com>
2025-01-14 23:29:31 +0700

    -

--- Ticker.ps1
+++ Ticker.ps1
@@ -1,5 +1,6 @@
-﻿# https://generic709.herokuapp.com/stockc/${singleticker}
-
+﻿
+# Martin Shkreli ticker API
+# https://generic709.herokuapp.com/stockc/${singleticker}
 Invoke-RestMethod "https://generic709.herokuapp.com/stockc/$($args[0])"
 
 
