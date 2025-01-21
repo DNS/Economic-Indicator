215e836b65586c008c2f4e702b9c763283bb8cea
DNS <37093+DNS@users.noreply.github.com>
2025-01-21 14:25:24 +0700

    A: Certificate of Deposit

---
+++ IDX.ps1
@@ -0,0 +1,9 @@
+﻿<#
+LPPF
+https://finance.yahoo.com/quote/LPPF.JK/
+https://www.msn.com/en-us/money/stockdetails/fi-bkgzjc?id=bkgzjc
+
+https://assets.msn.com/service/Finance/Quotes?apikey=0QfOX3Vn51YCzitbLaRkTTBadtWpgTN8NZLW0C1SEM&activityId=31FEC127-2063-40FB-AFD5-3FCB56BDD9DB&cm=en-us&it=web&scn=ANON&ids=bkgzjc&wrapodata=false
+#>
+
+
