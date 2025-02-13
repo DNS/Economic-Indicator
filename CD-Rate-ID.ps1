97d680705b7d313f96042a2c6ee8488ebe3766ef
DNS <37093+DNS@users.noreply.github.com>
2025-02-13 11:37:56 +0700

    Certificate of Deposit

--- CD-Rate-ID.ps1
+++ CD-Rate-ID.ps1
@@ -8,3 +8,10 @@ https://www.bca.co.id/id/Individu/produk/simpanan/Deposito-Berjangka
 bri
 #>
 
+$a -match '(?ims)<td>Rp 250.000.000 &ndash; &lt; Rp500.000.000</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>(.+?)%</td>'
+$cd_rate = $matches[1] / 100
+$cd_rate
+
+
+
+
