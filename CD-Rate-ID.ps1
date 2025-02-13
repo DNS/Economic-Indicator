4594a198b472abce31165bb51f28f52ba76ccf4c
DNS <37093+DNS@users.noreply.github.com>
2025-02-13 12:32:12 +0700

    Certificate of Deposit

--- CD-Rate-ID.ps1
+++ CD-Rate-ID.ps1
@@ -1,16 +1,16 @@
 ﻿<#
-danamon
-https://www.danamon.co.id/id/Personal/Lainnya/Suku-Bunga
+	danamon
+	https://www.danamon.co.id/id/Personal/Lainnya/Suku-Bunga
 
-bca
-https://www.bca.co.id/id/Individu/produk/simpanan/Deposito-Berjangka
+	bca
+	https://www.bca.co.id/id/Individu/produk/simpanan/Deposito-Berjangka
 
-bri
-https://bri.co.id/simulasi-deposito
+	bri
+	https://bri.co.id/simulasi-deposito
 #>
 
 # Danamon CD Rate
-$a = iwr https://www.danamon.co.id/id/Personal/Lainnya/Suku-Bunga
+$a = iwr 'https://www.danamon.co.id/id/Personal/Lainnya/Suku-Bunga'
 $m = $a.Content -match '(?ims)<td>Rp 250.000.000 &ndash; &lt; Rp500.000.000</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>(.+?)%</td>'
 if ($m) {
 	$cd_rate = $matches[1] / 100
--- CD-Rate-US.ps1
+++ CD-Rate-US.ps1
@@ -1,5 +1,21 @@
-﻿
-# wells fargo
+﻿<#
+	jpmorgan Chase
+	https://www.chase.com/content/dam/chase-ux/ratesheets/pdfs/rdfl1.pdf
+	
+	boa
+	https://media.bac-assets.com/DigitalDeposit_CA_CA_Los_Angeles.pdf
+	
+	wells fargo
+	https://www.wellsfargo.com/savings-cds/rates/
+#>
 
-# boa
+
+$a = irm -Uri 'https://www.chase.com/bin/services/cdRate' -Method Post -ContentType 'application/x-www-form-urlencoded; charset=UTF-8' -Body 'zipcode=90001&type=consumer&language=en-US&bankCode=703&regionCode=002'
+$cd_rate = ($a.ratesData.rates.'12-Month'[$a.ratesData.rates.'12-Month'.Count-1] -replace '%') / 100
+if ($cd_rate) {
+	$cd_rate
+}
+
+
+
 
