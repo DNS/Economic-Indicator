646c83132b1dd6ccbf0804b5f875a9c1a9ddab75
DNS <37093+DNS@users.noreply.github.com>
2025-02-13 11:42:22 +0700

    Certificate of Deposit

--- CD-Rate-ID.ps1
+++ CD-Rate-ID.ps1
@@ -6,8 +6,10 @@ bca
 https://www.bca.co.id/id/Individu/produk/simpanan/Deposito-Berjangka
 
 bri
+https://bri.co.id/simulasi-deposito
 #>
 
+# Danamon CD Rate
 $a -match '(?ims)<td>Rp 250.000.000 &ndash; &lt; Rp500.000.000</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>.+?</td>.+?<td>(.+?)%</td>'
 $cd_rate = $matches[1] / 100
 $cd_rate
