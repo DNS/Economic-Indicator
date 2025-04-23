e893bb5155acbff6561f2c01a618e21d0684d6ec
DNS <37093+DNS@users.noreply.github.com>
2025-04-24 01:40:56 +0700

    typo

--- RFI-Rate-ID.ps1
+++ RFI-Rate-ID.ps1
@@ -1,6 +1,6 @@
 ﻿# Interest-Rate-ID.ps1
 
-$a = Invoke-WebRequest https://www.bi.go.id/id/statistik/indikator/BI-Rate.aspx
+$a = iwr https://www.bi.go.id/id/statistik/indikator/BI-Rate.aspx
 $a.Content -match '<td class="text-center">(.*?) %</td>' | Out-Null
 "$($Matches[1])%"
 
