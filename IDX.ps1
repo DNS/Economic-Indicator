781629d5da99ab11a373067f6984ea1e84f727be
Daniel Sirait <37093+DNS@users.noreply.github.com>
2025-10-25 23:34:56 +0700

    Update IDX.ps1

--- IDX.ps1
+++ IDX.ps1
@@ -7,3 +7,28 @@ https://assets.msn.com/service/Finance/Quotes?apikey=0QfOX3Vn51YCzitbLaRkTTBadtW
 #>
 
 
+
+
+$a = iwr -Uri 'https://www.idx.co.id/primary/helper/GetStockChart?indexCode=LPPF&period=1Y' -UserAgent 'Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0' -Headers @{Referer='https://www.idx.co.id/'; Cookie='auth.strategy=local; __cf_bm=dUDB36UKDc58QfECQhLugJksocaHRIH2HxuF4RXlgOo-1761308125-1.0.1.1-4QXNlIkQlsKngnRHYgro__IRQAdvu8DjzCngm1PN2nf9iKb3zLpw1vD1dcbtDn2li8ybM21LxAxEMdmBJ8hB6kV_JiFvcxOwJ3rK3HFEqHQ; _cfuvid=CdCebvunrb.eIbV9Dr9gZmfvTAarTuZIoOhFmCbkAmA-1761308125105-0.0.1.1-604800000; EGRUM_BTM=ad5217ab-a7f8-4e6b-a5ca-8bae8ccdd517#~#1.B.11||0'}
+
+
+firefox --headless -dump-dom https://www.example.com
+
+
+/home/dns/bin/chrome-linux/chrome --no-sandbox --headless --disable-gpu --dump-dom https://example.com/
+
+
+/home/dns/bin/chrome-linux/chrome --no-sandbox --headless --window-size=412,892 --dump-dom 'https://www.idx.co.id/primary/helper/GetStockChart?indexCode=LPPF&period=1Y'
+
+# png screenshot
+/home/dns/bin/chrome-linux/chrome --no-sandbox --headless --disable-gpu --screenshot --window-size=412,892 --dump-dom https://example.com/
+
+# pdf output
+chrome --headless --print-to-pdf --no-pdf-header-footer https://developer.chrome.com/
+
+
+# --timeout
+chrome --headless --print-to-pdf --timeout=5000 https://developer.chrome.com/
+
+
+
